0c5d1ac1f70c7c5abdbc970142b7277412d87a80
rudal kunwar <ruzalkunwar@gmail.com>
2024-07-21 09:58:58 +0545

    java

--- java/test.java
+++ java/test.java
@@ -3,10 +3,14 @@ public class Test {
     public static void main(String[] args){
         try{
 
-            Class.forName("com.cj.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection('url','user','pass');
-    
+            // Class.forName("com.cj.mysql.jdbc.Driver");
+            // Connection con = DriverManager.getConnection('url','user','pass');
 
+
+            System.out.println("hello rudal");
+
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
